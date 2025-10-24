76f5ca3708f16365cd331b9545a5b618839b2695
Fdg1994 <frederik.de.gottal@gmail.com>
2025-10-24 15:02:40 +0200

    Add buttons for playerchoices

--- main.js
+++ main.js
@@ -68,5 +68,16 @@ function playRound(humanChoice = getHumanChoice(), computerChoice = getComputerC
     }
 }
 
-playRound();
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    playRound(button.id);
+  });
+});
+
+
+
+
 
